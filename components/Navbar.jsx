8c12ce12bf1288c67fbfcb5d89ae3480876d7491
uzohou <isaiahntina47@gmail.com>
2023-02-21 10:07:10 +0000

    new

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,6 +7,7 @@ import React, { useState } from 'react';
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+  const [active, setActive] = useState('home');
   return (
   <motion.nav
   variant={navVariants}
@@ -27,18 +28,19 @@ const Navbar = () => {
             initial: { scale: 1 },
             whileHover: { scale: 1.1 }
           }}
-          initial="initial"
-          whileHover="whileHover"
           className={`cursor-pointer font-poppins font-normal text-[-16px] 
             ${index === navlinks.length - 1 ? 'mr-0' : 'mr-10'} text-white`}
         >
+
           <a href={`#${nav.id}`}>{nav.title}</a>
+          
         </motion.li>        
         ))}
       </ul>
       <div className="sm:hidden flex flex-1 justify-end items-center">
       <img src={toggle ? closeIcon.Url : menuIcon.Url}
-      alt="menu" className="w-[28px] h-[28px] object-contain" onClick={()=> setToggle((prev) => !prev)}
+      alt="menu" className="w-[28px] h-[28px] object-contain cursor-pointer"
+      onClick={() => setToggle(!toggle)}
       />
       <div className={`${toggle? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20
       right-0 mx-4 my-2 min-w-[140px] rounded-x1 sidebar`}
@@ -56,7 +58,9 @@ const Navbar = () => {
           className={`font-poppins font-normal cursor-pointer text-[-16px] 
             ${index === navlinks.length - 1 ? 'mr-0' : 'mb-4'} text-white`}
         >
-          <a href={`#${nav.id}`}>{nav.title}</a>
+          <a href={`#${nav.id}`}>
+            {nav.title}
+            </a>
         </motion.li>        
         ))}
       </ul>

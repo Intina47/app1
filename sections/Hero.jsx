e1a45c535dfbf0cd94deb010b024d733a4e58c65
uzohou <isaiahntina47@gmail.com>
2023-02-21 09:33:36 +0000

    hero

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -36,7 +36,7 @@ const Hero = () => (
             className='w-full sm:h-[500px] h-[350px]
             object-cover rounded-tl-[140px] z-10 relative'
           />
-          <a href="#">
+          <a href="#explore">
             <div className="w-full flex justify-end sm:-mt-[70px]
             -mt-[50px] pr-[40px]  relative z-10">
               {/* create a certcular object with text book a both and an link arrow */}
@@ -47,11 +47,9 @@ const Hero = () => (
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-primary-black" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8l4 4m0 0l-4 4m4-4H3" />
                     </svg>
+                    </div>        
                     </div>
-                    
                     </div>
-                    </div>
-
             </div>
           </a>
       </motion.div>

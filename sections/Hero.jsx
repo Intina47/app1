8e89e577372b22ad0c1853c757ba33c8d6efb33e
uzohou <isaiahntina47@gmail.com>
2023-06-11 22:25:48 +0100

    video

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -2,9 +2,8 @@
 
 import {motion} from 'framer-motion';
 import Link from 'next/link';
-import React, { useState } from 'react';
 import styles from '../styles';
-import { slideIn, staggerContainer, textVariant,fadeIn } from '../utils/motion';
+import { slideIn, staggerContainer,fadeIn } from '../utils/motion';
 import { socials } from '../constants';
 
 const Hero = () => {
@@ -33,13 +32,13 @@ const Hero = () => {
         viewport={{ once: false, amount: 0.25 }}
         className={`${styles.innerWidth} mx-auto flex flex-col`}
       >
-       <motion.div variants={slideIn('left', 'tween', 0.2, 1)} className="flex flex-col gap-4">
-       <a href="https://goo.gl/maps/A6f372rpyHWAVSNeA">
-       <div className="flex items-center gap-2" title="Our Location">
-          {locationDetails.icon}
-          <h4 className="text-white text-[18px] font-bold">{locationDetails.details}</h4>
-        </div>
-        </a>
+        <motion.div variants={slideIn('left', 'tween', 0.2, 1)} className="flex flex-col gap-4">
+          <a href="https://goo.gl/maps/A6f372rpyHWAVSNeA">
+            <div className="flex items-center gap-2" title="Our Location">
+              {locationDetails.icon}
+              <h4 className="text-white text-[18px] font-bold">{locationDetails.details}</h4>
+            </div>
+          </a>
           {socials.map((social, index) => (
             <Link href={social.link} key={index} title={social.name}>
               <img
@@ -62,30 +61,32 @@ const Hero = () => {
         </div>
 
         <motion.div variants={slideIn('right', 'tween', 0.2, 1)} className="relative w-full md:-mt-[20px] -mt-[12px]">
-          <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]" />
-          <img
-            src="/afro3.png"
+          <div className="absolute w-full h-[300px]  rounded-tl-[140px] z-[0] -top-[30px]" />
+          <video
+            src="/hero_Vid.mp4"
             alt="hero"
-            className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
+            className="w-full sm:h-[500px] h-[340px] object-cover rounded-tl-[40px] z-10 relative"
+            autoPlay
+            muted
+            loop
+            style={{ filter: 'blur(0px)', animationDuration: '5s' }} // Apply CSS styles for blur and animation duration
           />
           <Link href="/reservation?type=Reservation">
-          <div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10">
-            <div className="flex flex-col items-center gap-2">
-              <div className="flex flex-row items-center gap-2">
-                <h1 className="text-white font-bold text-[18px]">Make a Booth Reservation</h1>
-                <div
-                  className="bg-secondary-green rounded-full w-[50px] h-[50px] flex items-center justify-center"
-                  // onClick={handleReservationClick}
-                >
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-primary-black" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8l4 4m0 0l-4 4m4-4H3" />
-                  </svg>
+            <div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10">
+              <div className="flex flex-col items-center gap-2">
+                <div className="flex flex-row items-center gap-2">
+                  <h1 className="text-white font-bold text-[18px]">Make a Booth Reservation</h1>
+                  <div className="bg-secondary-green rounded-full w-[50px] h-[50px] flex items-center justify-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-primary-black" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                    </svg>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
           </Link>
         </motion.div>
+
       </motion.div>
 
       {/* arrow down */}

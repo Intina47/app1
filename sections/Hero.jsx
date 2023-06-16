e4d261c906da1415b8f41ca4317f10d725e4a56a
uzohou <isaiahntina47@gmail.com>
2023-06-16 01:41:22 +0100

    rounded_bottom

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -88,7 +88,7 @@ const Hero = () => {
           <video
             src="/hero_Vid.mp4"
             alt="hero"
-            className="w-full sm:h-[500px] h-[340px] object-cover rounded-tl-[40px] z-10 relative"
+            className="w-full sm:h-[500px] h-[340px] object-cover rounded-tl-[40px] rounded-bl-[40px] z-10 relative"
             autoPlay
             muted
             loop

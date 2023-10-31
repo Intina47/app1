926fefe64defe9c26201b10ac27f0d6f0b3e557e
uzohou <isaiahntina47@gmail.com>
2023-10-31 00:22:07 +0000

    lazy loading

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+// layout.js
 import '../styles/globals.css';
 //eslint-disable-next-line import/no-unresolved
 import { Analytics } from '@vercel/analytics/react';
@@ -5,6 +6,37 @@ import { Analytics } from '@vercel/analytics/react';
 const RootLayout = ({ children }) => (
   <html lang="en">
     <head>
+      <meta charSet="utf-8" />
+      {/* SEO for this night club website */}
+      <title>Afrobeats Dundee Basement Night Club</title>
+      <meta name="description" content="Welcome to Afrobeats Dundee Basement Night Club in Dundee, Scotland. We offer the best nightlife experience with top DJs and live music." />
+      <meta name="keywords" content="
+      afrobeatsdundee.co.uk, afrobeatsdundee.com, Afrobeats, Dundee, Night Club, Nightlife, DJ, Music, Live Music, Night Club Dundee, Night Club Scotland, Night Club UK, Night Club Europe, Night Club World,
+       Night life,Night life Dundee, Night life Scotland, Night life UK, visit scotland, visit dundee, visit edinbrugh, visit glasgow, most hidden places in dundee, underground"
+      />
+      <meta name="author" content="Afro beats Dundee" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+      {/* Open Graph for this night club website */}
+      <meta property="og:title" content="Afrobeats Dundee Basement Night Club" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://afrobeatsdundee.co.uk" />
+      <meta property="og:description" content="Welcome to Afrobeats Dundee Basement Night Club in Dundee, Scotland. We offer the best nightlife experience with top DJs and live music." />
+      <meta property="og:site_name" content="Afrobeats Dundee Basement Night Club" />
+
+      {/* Twitter for this night club website */}
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content="Afrobeats Dundee Basement Night Club" />
+      <meta name="twitter:description" content="Welcome to Afrobeats Dundee Basement Night Club in Dundee, Scotland. We offer the best nightlife experience with top DJs and live music." />
+      <meta name="twitter:site" content="@afrobeatsdundee" />
+      <meta name="twitter:creator" content="@afrobeatsdundee" />
+
+      {/* Favicon for this night club website */}
+      <link rel="icon" href="public/afrobeats-logo2.png" />
+      <link rel="apple-touch-icon" href="public/afrobeats-logo2.png" />
+      {/* Fonts for this night club website */}
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+
       <link rel="preconnect" href="https://stijndv.com" />
       <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
     </head>
--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Suspense } from 'react';
 import { Footer, Navbar } from '../components';
 import { About, Explore, GetStarted, Hero } from '../sections';
 import OurMenuPopup from '../components/menuPopup';
@@ -28,39 +28,41 @@ const Page = () => {
 
   return (
     <div className="bg-black overflow-hidden">
-      <div className="relative z-10 position-sticky">
-        <Navbar />
-      </div>
-      <div className="relative z-0">
-        <Hero />
-      </div>
-      <div className="relative z-0">
-        <About />
-        <div className="gradient-03 z-10" />
-        <Explore />
-      </div>
-      <div className="relative">
-        <GetStarted />
-        <div className="gradient-04 z-0" />
-      </div>
-      <div className="relative">
-        <Map />
-        <div className="gradient-05 z-0" />
-      </div>
-      <Footer />
-      {/* Floating button when clicked show the menu */}
-      <div className="floating-button">
-        <button className="menu-float-button" onClick={(event) => togglePopup(event)}>
-          <img src={chevron.Url} alt={chevron.name} className="menu-icon" />
-          <span className="menu-icon">OUR MENU</span>
-        </button>
-      </div>
-      {/* Only show the popup if the showPopup state is true */}
-      {showPopup && (
+      <Suspense fallback={<div>Afro Beats Loading...</div>}>
+        <div className="relative z-10 position-sticky">
+          <Navbar />
+        </div>
+        <div className="relative z-0">
+          <Hero />
+        </div>
+        <div className="relative z-0">
+          <About />
+          <div className="gradient-03 z-10" />
+          <Explore />
+        </div>
+        <div className="relative">
+          <GetStarted />
+          <div className="gradient-04 z-0" />
+        </div>
+        <div className="relative">
+          <Map />
+          <div className="gradient-05 z-0" />
+        </div>
+        <Footer />
+        {/* Floating button when clicked show the menu */}
+        <div className="floating-button">
+          <button className="menu-float-button" onClick={(event) => togglePopup(event)}>
+            <img src={chevron.Url} alt={chevron.name} className="menu-icon" />
+            <span className="menu-icon">OUR MENU</span>
+          </button>
+        </div>
+        {/* Only show the popup if the showPopup state is true */}
+        {showPopup && (
         <div className="popup-overlay ">
           <OurMenuPopup onClose={(event) => togglePopup(event)} />
         </div>
       )}
+      </Suspense>
     </div>
   );
 };

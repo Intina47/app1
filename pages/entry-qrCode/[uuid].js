6cced7a9831c18e02261b3c82d7c4d02a6e306ff
uzohou <isaiahntina47@gmail.com>
2023-11-14 01:54:16 +0000

    oops

--- pages/entry-qrCode/[uuid].js
+++ pages/entry-qrCode/[uuid].js
@@ -32,9 +32,28 @@ const entryqrcode = () => {
 
   if (!isValid) {
     return (
-      <div>
-        <h1>AFROBEATS DUNDEE ENTRY QR CODE</h1>
-        <p>!! QR Code not found</p>
+      <div className="flex items-center justify-center h-screen">
+        <div className="text-center animate-zoomInOut">
+          <h1 className="text-3xl font-bold mb-4 text-primary">AFROBEATS DUNDEE ENTRY QR CODE</h1>
+          <div className="relative">
+            <div className="absolute bg-white border border-gray-300 h-4 w-4 rotate-45 -top-2 -left-2" />
+            <p className="text-xl mb-4">
+              <span className="text-red-500">Oops!! Gotcha,</span> <br />
+              <span className="text-yellow-500">You almost got me there, That was close.</span> <br />
+              <span className="text-blue-500">Looks like the <span className="text-green-500"> QR Code url </span> is feeling a bit shy today.</span><br />
+              <span className="text-green-500">Double-check it or consider</span>
+            </p>
+          </div>
+          <button
+            type="button"
+            className="text-green-500 bg-black-500 border-2 border-primary hover:bg-primary-dark px-6 py-3 rounded-full transition duration-300 focus:outline-none"
+            onClick={() => {
+      window.location.href = 'https://afrobeatsdundee.co.uk/membership';
+    }}
+          >
+            signing up for membership
+          </button>
+        </div>
       </div>
     );
   }

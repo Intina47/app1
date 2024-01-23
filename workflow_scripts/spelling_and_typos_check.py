189535a76248be5624e84a7403c4c02adc188d77
mamba <isaiahntina47@gmail.com>
2024-01-23 13:39:10 +0000

    Add codespell and spelling check script

---
+++ workflow_scripts/spelling_and_typos_check.py
@@ -0,0 +1,11 @@
+import subprocess
+import os
+
+def run_codespell():
+    root_dir = os.getcwd()  # Get the current working directory
+
+    # Run codespell with the root directory as the target
+    subprocess.run(['codespell', root_dir])
+
+if __name__ == "__main__":
+    run_codespell()

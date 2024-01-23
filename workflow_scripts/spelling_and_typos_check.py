218ee4168a4d28bef06cfda79462cf57a28dbb08
mamba <isaiahntina47@gmail.com>
2024-01-23 14:31:52 +0000

    Fix codespell command to write changes to files

--- workflow_scripts/spelling_and_typos_check.py
+++ workflow_scripts/spelling_and_typos_check.py
@@ -5,7 +5,7 @@ def run_codespell():
     root_dir = os.getcwd()  # Get the current working directory
 
     # Run codespell with the root directory as the target
-    subprocess.run(['codespell', root_dir])
+    subprocess.run(['codespell', root_dir, '--write-changes'])
 
 if __name__ == "__main__":
     run_codespell()

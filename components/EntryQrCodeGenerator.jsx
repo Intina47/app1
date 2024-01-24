a6bf77407d063a45cf22562118e093bba0a57157
mamba <isaiahntina47@gmail.com>
2024-01-24 03:56:03 +0000

    isssue 24

--- components/EntryQrCodeGenerator.jsx
+++ components/EntryQrCodeGenerator.jsx
@@ -6,7 +6,6 @@ import { socials } from '../constants';
 
 const QRCodeGenerator = ({ uuid }) => {
         // just a variable to hold the uuid to remove the warning for now
-        console.log(uuid, 'Generated QR Code');
         const link = 'https://afrobeatsdundee.co.uk/membership';
         const [copied, setCopied] = useState(false);
         const [countdownTime, setCountdownTime] = useState(() => {

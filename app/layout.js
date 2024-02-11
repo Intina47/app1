e207109a372172f4a843b99e737dcd177bd6ea25
mamba <isaiahntina47@gmail.com>
2024-02-11 08:03:21 +0000

    Refactor schema markup in layout.js

--- app/layout.js
+++ app/layout.js
@@ -30,8 +30,7 @@ const RootLayout = ({ children }) => (
       <meta property="og:url" content="https://afrobeatsdundee.co.uk/" />
       <meta property="og:site_name" content="Afrobeats Dundee Basement Night Club" />
       <meta name="google-site-verification" content="RnKKePD0sQjxnouuLaqu_nNvyo5K7JZt6U3btvdqi2Y" />
-      {/* Schema Markup */}
-      <script type="application/ld+json">
+      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify({
         '@context': 'https://schema.org',
         '@type': 'Organization',
         name: 'Afrobeats Dundee Basement Night Club',
@@ -39,38 +38,39 @@ const RootLayout = ({ children }) => (
         url: 'https://afrobeatsdundee.co.uk',
         logo: 'https://afrobeatsdundee.co.uk/afrobeats-logo2.png',
         sameAs: [
-        'https://www.facebook.com/afrobreatsdundee',
-        'https://www.instagram.com/afrobeatsdundee/',
-        'https://www.snapchat.com/add/afrobeatsdundee',
+          'https://www.facebook.com/afrobreatsdundee',
+          'https://www.instagram.com/afrobeatsdundee/',
+          'https://www.snapchat.com/add/afrobeatsdundee',
         ],
-        contactPoint: [
-        '@type': 'ContactPoint',
-        telephone: '+441 382 782 663',
-        contactType: 'customer service',
-        ],
-        address: [
-        '@type': 'PostalAddress',
-        streetAddress: '3 Session Street',
-        addressLocality: 'Dundee',
-        postalCode: 'DD15',
-        addressCountry: 'UK',
-        ],
-        "geo": [
-        "@type": "GeoCoordinates",
-        "latitude": 56.460455599999996,
-        "longitude": -2.9801999
-        ],
-        "openingHoursSpecification": [
-        "@type": "OpeningHoursSpecification",
-        "dayOfWeek": [
-        "Tuesday",
-        "Friday",
-        "Saturday"
-        ],
-        "opens": "20:00",
-        "closes": "02:30"
-        ]
-      </script>
+        contactPoint: {
+          '@type': 'ContactPoint',
+          telephone: '+441 382 782 663',
+          contactType: 'customer service',
+        },
+        address: {
+          '@type': 'PostalAddress',
+          streetAddress: '3 Session Street',
+          addressLocality: 'Dundee',
+          postalCode: 'DD15',
+          addressCountry: 'UK',
+        },
+        geo: {
+          '@type': 'GeoCoordinates',
+          latitude: 56.460455599999996,
+          longitude: -2.9801999,
+        },
+        openingHoursSpecification: {
+          '@type': 'OpeningHoursSpecification',
+          dayOfWeek: [
+            'Tuesday',
+            'Friday',
+            'Saturday',
+          ],
+          opens: '20:00',
+          closes: '02:30',
+        },
+      }) }}
+      />
     </head>
     <body>
       {children}

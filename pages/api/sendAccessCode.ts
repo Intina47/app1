0ffc8ed0a194c4c11b5e8b9e077b3f6bc4270c63
mamba <isaiahntina47@gmail.com>
2024-02-11 23:33:55 +0000

    Commented out code and fixed email sending

--- pages/api/sendAccessCode.ts
+++ pages/api/sendAccessCode.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { connectToDatabase } from '../../utils/database';
-import { generateCode } from '../../utils/codeGenerator.ts';
+// import { generateCode } from '../../utils/codeGenerator.ts';
 import { sendEmail } from '../../utils/emailer.ts';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -8,15 +8,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     res.setHeader('Cache-Control', 'no-store');
     // const ip = req.headers['x-real-ip'] as string || req.socket.remoteAddress as string || req.headers['x-forwarded-for'] as string;
     // i want to be able to get ip even if in vercel
-    const ip = '';
+    const ip = '1234-567-89';
     // Generate a new code and send it
-    const code = generateCode();
+    // const code = generateCode();
+    const code = '729240';
     try {
     sendEmail(code);
-    } catch (error) {
-        res.status(500).json({ message: 'Failed to send email' });
-        return;
-    }
 
     // Update the user document with the new code and set the codeGenerating flag to false
     // Update the user document with the new code and set the codeGenerating flag to false
@@ -25,11 +22,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         { $set: { code, codeGenerating: false } },
         { returnOriginal: false, upsert: true },
     );
-
     if (!updatedUser.value) {
-        res.status(500).json({ message: 'Error saving user'});
+        res.status(500).json({ message: 'Failed to update user' });
         return;
     }
-
-    res.status(200).json({ messsage: 'Code sent'});
+    res.status(200).json({ message: 'Email sent' });
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to send email' });
+    }
 };

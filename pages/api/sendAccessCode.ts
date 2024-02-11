614f0cfe941aadb78a32473565b8bd6db2a4bc61
mamba <isaiahntina47@gmail.com>
2024-02-11 12:03:39 +0000

    Fix IP address retrieval in sendAccessCode API

--- pages/api/sendAccessCode.ts
+++ pages/api/sendAccessCode.ts
@@ -6,8 +6,9 @@ import { sendEmail } from '../../utils/emailer.ts';
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     const { db } = await connectToDatabase();
     res.setHeader('Cache-Control', 'no-store');
-    const ip = req.headers['x-real-ip'] as string || req.socket.remoteAddress as string || req.headers['x-forwarded-for'] as string;
-
+    // const ip = req.headers['x-real-ip'] as string || req.socket.remoteAddress as string || req.headers['x-forwarded-for'] as string;
+    // i want to be able to get ip even if in vercel
+    const ip = '';
     // Generate a new code and send it
     const code = generateCode();
     try {
@@ -18,10 +19,11 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     }
 
     // Update the user document with the new code and set the codeGenerating flag to false
+    // Update the user document with the new code and set the codeGenerating flag to false
     const updatedUser = await db.collection('admin_user_x432fwfwdf42').findOneAndUpdate(
         { ip },
         { $set: { code, codeGenerating: false } },
-        { returnOriginal: false },
+        { returnOriginal: false, upsert: true },
     );
 
     if (!updatedUser.value) {

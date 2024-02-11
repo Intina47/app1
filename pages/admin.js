dde33943743abe2aa6885edefeb31469c69d97a4
mamba <isaiahntina47@gmail.com>
2024-02-11 07:42:32 +0000

    Refactor admin page component and fix code verification bug

--- pages/admin.js
+++ pages/admin.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
-import UploadPromotion from '../components/admin/uploadPromotions.tsx';
-import '../styles/globals.css';
 import { toast , ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import UploadPromotion from '../components/admin/uploadPromotions.tsx';
+import '../styles/globals.css';
 
 const AdminPage = () => {
   const [code, setCode] = React.useState('');
@@ -20,14 +20,14 @@ const AdminPage = () => {
         })
         .catch((error) => {
           toast.error('Something went wrong. Please refresh the page and try again.');
+          console.log(error);
         });
     }, []);
-
 
   const verifyCode = async (e) => {
     e.preventDefault();
     const res = await axios.post('/api/verifyCode', { code });
-    if(res.data.accessGranted){
+    if (res.data.accessGranted){
       setAccessGranted(true);
     } else {
       toast.error('Invalid access code');
@@ -37,24 +37,28 @@ const AdminPage = () => {
 
   if (!accessGranted) {
     return (
-      <><ToastContainer /><div className="flex items-center justify-center h-screen">
-        <form onSubmit={verifyCode} className="flex flex-col space-y-4">
-          <input
-            type="text"
-            placeholder="Enter access code"
-            value={code}
-            onChange={(e) => setCode(e.target.value)}
-            className="border-2 border-black p-2" />
-          <button type="submit" className="bg-black text-white p-2 rounded-md">Verify</button>
-        </form>
-      </div></>
+      <>
+        <ToastContainer />
+        <div className="flex items-center justify-center h-screen">
+          <form onSubmit={verifyCode} className="flex flex-col space-y-4">
+            <input
+              type="text"
+              placeholder="Enter access code"
+              value={code}
+              onChange={(e) => setCode(e.target.value)}
+              className="border-2 border-black p-2"
+            />
+            <button type="submit" className="bg-black text-white p-2 rounded-md">Verify</button>
+          </form>
+        </div>
+      </>
     );
   }
 
   return (
-  <div>
-    <UploadPromotion />
-  </div>
+    <div>
+      <UploadPromotion />
+    </div>
   );
 };
 

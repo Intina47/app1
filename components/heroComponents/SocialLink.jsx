0b37d4c942985a4b320c5742c19d13cad0824446
mamba <isaiahntina47@gmail.com>
2024-02-12 05:07:09 +0000

    Refactor SocialLink component to include social name

--- components/heroComponents/SocialLink.jsx
+++ components/heroComponents/SocialLink.jsx
@@ -1,14 +1,16 @@
-// path:components/heroComponents/SocialLink.jsx
 import Link from 'next/link';
 
 const SocialLink = ({ social }) => (
-  <Link href={social.url} title={social.name}>
-    <img
-      key={social.name}
-      src={social.url}
-      alt={social.name}
-      className="w-[24px] h-[24px] object-contain cursor-pointer font-bold text-xl transform hover:scale-105 transition duration-300 relative overflow-hidden"
-    />
+  <Link href={social.link} title={social.name}>
+    <div className="flex items-center">
+      <img
+        key={social.name}
+        src={social.url}
+        alt={social.name}
+        className="w-[24px] h-[24px] object-contain cursor-pointer font-bold text-xl transform hover:scale-105 transition duration-300 relative overflow-hidden"
+      />
+      <h4 className="text-white text-sm loc_underline-on-hover ml-2">{social.name}</h4>
+    </div>
   </Link>
 );
 

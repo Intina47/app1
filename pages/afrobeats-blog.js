17150ea3122ab3d99ad0ed80eaf89639129864e9
mamba <isaiahntina47@gmail.com>
2024-02-14 05:12:29 +0000

    Commented out import and component usage of BlogPage in afrobeats-blog.js

--- pages/afrobeats-blog.js
+++ pages/afrobeats-blog.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import BlogPage from '../components/blog/ourBlog';
+// import BlogPage from '../components/blog/ourBlog';
 import '../styles/globals.css';
 import { Footer, Navbar } from '../components';
 
 const AfroblogPage = () => (
   <div className="bg-primary-black overflow-hidden">
     <Navbar />
-    <BlogPage />
+    {/* <BlogPage /> */}
     <Footer />
   </div>
         );

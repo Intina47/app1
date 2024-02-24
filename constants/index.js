e143d556be01730fe383908578dd74e1657c1bcd
mamba <isaiahntina47@gmail.com>
2024-02-24 05:53:26 +0000

    Update cocktail menu URL

--- constants/index.js
+++ constants/index.js
@@ -32,13 +32,7 @@ export const menu = [
     id: 'menu-1',
     title: 'Cocktails',
     description: 'Cocktails',
-    url: '/https://firebasestorage.googleapis.com/v0/b/afrobeatsdundee-22828.appspot.com/o/events%2F7b583a1e-7c5c-4f55-90c3-02eb90ffdc0b?alt=media&token=58741870-f4d8-460f-9df5-026a06755a29',
-  },
-  {
-    id: 'menu-2',
-    title: 'Bottle Menu',
-    description: 'Bottle Menu',
-    url: '/cocktail_menu.png',
+    url: 'https://firebasestorage.googleapis.com/v0/b/afrobeatsdundee-22828.appspot.com/o/events%2F7b583a1e-7c5c-4f55-90c3-02eb90ffdc0b?alt=media&token=58741870-f4d8-460f-9df5-026a06755a29',
   },
 ];
 

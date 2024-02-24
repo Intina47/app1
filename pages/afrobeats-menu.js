3055d7460c8f17a3751a369833042caee1267885
mamba <isaiahntina47@gmail.com>
2024-02-24 05:45:04 +0000

    Refactor afrobeats-menu.js component

--- pages/afrobeats-menu.js
+++ pages/afrobeats-menu.js
@@ -5,14 +5,14 @@ import { Footer, Navbar } from '../components';
 import '../styles/globals.css';
 
 const menu = () => (
-    <div className=" overflow-hidden">
-        <Navbar />
-        <div className="flex justify-center items-center h-20 bg-opacity-50">
-            <h1 className="text-4xl font-bold text-white">AfroBeats Menu</h1>
-        </div>
-        <MembershipForm />
-        <Footer />
+  <div className=" overflow-hidden">
+    <Navbar />
+    <div className="flex justify-center items-center h-20 bg-opacity-50">
+      <h1 className="text-4xl font-bold text-white">AfroBeats Menu</h1>
     </div>
+    <MembershipForm />
+    <Footer />
+  </div>
     );
 
 export default menu;

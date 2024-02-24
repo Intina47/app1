28473037766b206d814bbeadac8a0fe89605eb0b
mamba <isaiahntina47@gmail.com>
2024-02-24 05:44:56 +0000

    Refactor menuComponent to use arrow function syntax

--- components/menu/menuComponent.jsx
+++ components/menu/menuComponent.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
-
-const MenuComponent = () => {
-    return (
-        <div className="flex justify-center  items-center h-screen">
-            <img className="w-full h-full object-scale-down" src="https://firebasestorage.googleapis.com/v0/b/afrobeatsdundee-22828.appspot.com/o/events%2F7b583a1e-7c5c-4f55-90c3-02eb90ffdc0b?alt=media&token=58741870-f4d8-460f-9df5-026a06755a29" alt="Menu image" />
-        </div>
-    );
-};
+const MenuComponent = () => (
+  <div className="flex justify-center  items-center h-screen">
+    <img className="w-full h-full object-scale-down" src="https://firebasestorage.googleapis.com/v0/b/afrobeatsdundee-22828.appspot.com/o/events%2F7b583a1e-7c5c-4f55-90c3-02eb90ffdc0b?alt=media&token=58741870-f4d8-460f-9df5-026a06755a29" alt="AfroBeats Menu" />
+  </div>
+);
 
 export default MenuComponent;

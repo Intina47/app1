4c21b5d21065fed965e030ff8d1ff71b13187bf5
mamba <isaiahntina47@gmail.com>
2024-04-30 13:35:38 +0000

    Refactor useFetchEvents hook to include loading state

--- hooks/useFetchEvents.js
+++ hooks/useFetchEvents.js
@@ -1,27 +1,27 @@
+// path: hooks/useFetchEvents.js
 import { useState, useEffect } from 'react';
 
 const useFetchEvents = () => {
     const [events, setEvents] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-    useEffect(
-() => {
-        fetch('/api/events')
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-                return response.json();
-            })
-            .then((data) => {
-                console.log('data', data);
-                setEvents(data);
-            })
-            .catch((error) => console.error('Error:', error));
-    },
-     [],
-);
+    useEffect(() => {
+                fetch('/api/events')
+                    .then((response) => {
+                        if (!response.ok) {
+                            throw new Error(`HTTP error! status: ${response.status}`);
+                        }
+                        return response.json();
+                    })
+                    .then((data) => {
+                        console.log('data', data);
+                        setEvents(data);
+                        setLoading(false);
+                    })
+                    .catch((error) => console.error('Error:', error));
+            },[]);
 
-    return events;
+    return { events, loading };
 };
 
 export default useFetchEvents;
